d8389ee57719af9acbe34cea75a92d2e166e084d
Franco Lopez Paviolo <francomlopez@mi.unc.edu.ar>
2020-06-06 18:39:00 -0300

    funcionalidad displays

--- build/default/production/ED2TPFINAL_src.o.d
+++ build/default/production/ED2TPFINAL_src.o.d
@@ -1,3 +1,3 @@
 build/default/production/ED2TPFINAL_src.o:  \
-	C:/Users/nacho/Documents/Facultad/4to/ed2/MPLab\ Projects/ED2TPFINAL.X/ED2TPFINAL_src.asm  \
+	E:/Documentos/facu/4to\ año/ELECTRONICA\ DIGITAL\ 2/TPFINAL/ED2TPFINAL/ED2TPFINAL_src.asm  \
 
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=ED2TPFINAL.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/ED2TPFINAL.X.production.hex
+CND_ARTIFACT_NAME_default=ED2TPFINAL.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ED2TPFINAL.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=ed2tpfinal.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ed2tpfinal.x.tar
+CND_PACKAGE_NAME_default=ed2tpfinal.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ed2tpfinal.tar
